08de33540658e920a24b33be87f4312a34b59e06
Rachel Kang <rachel.j.kang@gmail.com>
2020-12-01 16:31:25 -0500

    Remove /bin /obj

---
+++ recipeSearchFlyout/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace recipeSearchFlyout.Models
+{
+	public class Recipe
+	{
+        public string Id { get; set; }
+        public string RecipeName { get; set; }
+        public string ImageUrl { get; set; }
+        public string Ingredients { get; set; }
+        public string RecipeBody { get; set; }
+        public FormattedString RecipeUrl { get; set; }
+    }
+}
